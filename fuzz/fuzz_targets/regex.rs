3d51d878fb9e43e4e9ce351a8d652aab0ea62799
Boshen <boshenc@gmail.com>
2026-07-03 08:26:39 +0800

    fix: update oxc 0.138 compatibility

--- fuzz/fuzz_targets/regex.rs
+++ fuzz/fuzz_targets/regex.rs
@@ -1,21 +1,13 @@
 #![no_main]
 
 use oxc::allocator::Allocator;
-use oxc_regular_expression::{ParserOptions, PatternParser};
+use oxc_regular_expression::{LiteralParser, Options};
 
 libfuzzer_sys::fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         if s.chars().all(|s| !s.is_control()) {
             let allocator = Allocator::default();
-            let _ = PatternParser::new(
-                &allocator,
-                &s,
-                ParserOptions {
-                    span_offset: 0,
-                    unicode_mode: true,
-                    unicode_sets_mode: true,
-                }
-            ).parse();
+            let _ = LiteralParser::new(&allocator, s, Some("v"), Options::default()).parse();
         }
     }
 });
